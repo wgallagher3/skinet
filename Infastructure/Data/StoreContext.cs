29f6b40755da0dafa688d3559a4bc51634b6f743
William Gallagher <wgallagher3@gmail.com>
2020-04-26 17:10:23 -0500

    Initial Commit

---
+++ Infastructure/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
